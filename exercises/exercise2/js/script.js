c3ce6020a8bde433889299f21d0c9dc94f9dabfd
Miha <m_eftene@cda069ev5815x.local>
2019-09-26 17:09:45 -0400

    EX: Added other if statements and a counter to the winning background.

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -34,21 +34,22 @@ let enemySizeIncrease = 3;
 let enemySpeed = 2;
 let enemyVX = 5;
 
-//The text and score that will be displayed//
-let displayText;
-let displayScore;
-
 // How many hours the player has made (hours of Sleep)
 let hours = 0;
 
 //Declare background Images
 let dayBg;
 let sunriseBg;
-let nightBg
+let nightBg;
+let winBg;
 
 //Declare our characters
 let rabbitPlayer;
 let rabbitEnemy;
+
+//Add counter for the winning background (frames)
+let counter = 300;
+
 
 //meant to load images inside the game already
 function preload() {
@@ -58,37 +59,54 @@ function preload() {
   dayBg = loadImage("assets/images/backgroundDay.png");
   sunriseBg = loadImage("assets/images/backgroundSunrise.png");
   nightBg = loadImage("assets/images/backgroundNight.png");
+  winBg = loadImage("assets/images/winBg.png");
 }
+
+
 // setup()
 //
 // Make the canvas, position the avatar and anemy
 function setup() {
   // Create our playing area
-  createCanvas(500,500);
+  createCanvas(500, 500);
 
   // Put the rabbit player in the centre
-  avatarX = width/2;
-  avatarY = height/2;
+  avatarX = width / 2;
+  avatarY = height / 2;
 
   // Put the rabbit enemy to the left at a random y coordinate within the canvas
   enemyX = 0;
-  enemyY = random(0,height);
+  enemyY = random(0, height);
 
   // No stroke so it looks cleaner
   noStroke();
-
 }
+
 
 // draw()
 //
 // Handle moving the rabbit player, rabbit enemy and checking for hours and
 // game over situations.
 function draw() {
+  //draw the "winning background"//
+  if (hours >= 12 && counter > 0) {
+    image(winBg, 0, 0, width, height);
+    enemyX = 0;
+    enemyY = random(0, height);
+    avatarX = width / 2;
+    avatarY = height / 2;
+    counter -= 1;
+    // if statement for the counter to go back to the scene
+    if (counter <= 0) {
+      hours = 0;
+      counter = 300;
+      enemySpeed = 2;
+    }
+    return
+  }
+
   // Adds the initial image (day)
   //background(255,220,220);
-
-  //
-
 
   // Default the rabbit player's velocity to 0 in case no key is pressed this frame
   avatarVX = 0;
@@ -100,8 +118,7 @@ function draw() {
   // Left and right
   if (keyIsDown(LEFT_ARROW)) {
     avatarVX = -avatarSpeed;
-  }
-  else if (keyIsDown(RIGHT_ARROW)) {
+  } else if (keyIsDown(RIGHT_ARROW)) {
     avatarVX = avatarSpeed;
   }
 
@@ -109,8 +126,7 @@ function draw() {
   // horizontally at the same time)
   if (keyIsDown(UP_ARROW)) {
     avatarVY = -avatarSpeed;
-  }
-  else if (keyIsDown(DOWN_ARROW)) {
+  } else if (keyIsDown(DOWN_ARROW)) {
     avatarVY = avatarSpeed;
   }
 
@@ -126,15 +142,15 @@ function draw() {
   // Check if the rabbit enemy and player overlap - if they do the player loses
   // We do this by checking if the distance between the centre of the enemy
   // and the centre of the avatar is less that their combined radii
-  if (dist(enemyX,enemyY,avatarX,avatarY) < enemySize/2 + avatarSize/2) {
+  if (dist(enemyX, enemyY, avatarX, avatarY) < enemySize / 2 + avatarSize / 2) {
     // Tell the rabbit player they lost
     console.log("YOU LOSE!");
     // Reset the enemy's position
     enemyX = 0;
-    enemyY = random(0,height);
+    enemyY = random(0, height);
     // Reset the rabbit player's position
-    avatarX = width/2;
-    avatarY = height/2;
+    avatarX = width / 2;
+    avatarY = height / 2;
     // Reset the dodge counter
     hours = 0;
     //Reset the rabbit enemy's size and Speed
@@ -147,9 +163,9 @@ function draw() {
     // If they went off the screen they lose in the same way as above.
     console.log("YOU LOSE!");
     enemyX = 0;
-    enemyY = random(0,height);
-    avatarX = width/2;
-    avatarY = height/2;
+    enemyY = random(0, height);
+    avatarX = width / 2;
+    avatarY = height / 2;
     hours = 0;
   }
 
@@ -161,7 +177,7 @@ function draw() {
     console.log(hours + " hours!");
     // Reset the rabbit enemy's position to the left at a random height
     enemyX = 0;
-    enemyY = random(0,height);
+    enemyY = random(0, height);
     //Increase the speed and size after a number certain number of hours
     enemySpeed += enemyIncreaseSpeed;
     enemySize += enemySizeIncrease;
@@ -176,25 +192,24 @@ function draw() {
   // Adds a condition: Once the player reaches a number of hours the scene changes...
 
   //Scene is being switched to sunrise
-  if(hours>=6){
-  image(sunriseBg, 0,0, width, height);
-}
-//Scene is being switched to day
-if(hours>=10){
-  image(dayBg, 0, 0, width, height);
-}
-
+  if (hours >= 6) {
+    image(sunriseBg, 0, 0, width, height);
+  }
+  //Scene is being switched to day
+  if (hours >= 10) {
+    image(dayBg, 0, 0, width, height);
+  }
 
   //  Draw the number of hours slept(score)-Center top
-      fill(0);
-      text(hours, width/2-8, height/8, width/20, height/8);
-      textFont("impact");
-      textSize(50);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  text(hours + " hours passed", width / 2 - 8, height / 8);
+  textFont("impact");
+  textSize(36);
 
   // Draw the rabbit player
-  image(rabbitPlayer,avatarX,avatarY,avatarSize,avatarSize);
+  image(rabbitPlayer, avatarX, avatarY, avatarSize, avatarSize);
 
   // Draw the enemy rabbit
-  image(rabbitEnemy,enemyX,enemyY,enemySize,enemySize);
-
+  image(rabbitEnemy, enemyX, enemyY, enemySize, enemySize);
 }
