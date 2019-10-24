293bd3ec2212a8ec705dec0db33dea5102a98eb3
mihaeftene <mihaeftene@users.noreply.github.com>
2019-10-24 11:44:19 -0400

    Ex5: Added my 2 new predators (pokemonGirl and pokemonBoy) and gave them two different commands so one can move with WASD and the other up, down, right and left keys

--- exercises/exercise5/js/script.js
+++ exercises/exercise5/js/script.js
@@ -1,12 +1,12 @@
 // Predator-Prey Simulation
 // by Mihaela Eftene
 //
-// Creates a predator and three prey (of different sizes and speeds)
-// The predator chases the prey using the arrow keys and consumes them.
-// The predator loses health over time, so must keep eating to survive.
+// Gotta catch em all!? Exactly! Play as either one of the characters and catch as many Pokemons as you can!
 
-// Our predator
-let tiger;
+
+//declare variables for our two new predators
+let pokemonBoy;
+let pokemonGirl;
 
 // The three prey
 let antelope;
@@ -19,7 +19,12 @@ let bee;
 // Creates objects for the predator and three prey
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
+  //adding our two new predators (pokemonBoy & pokemonGirl)
+  //we will move pokemonGirl with WASD keys
+  pokemonGirl = new Predator(250, 250, 5, color(203, 156, 203), 40, 87, 83, 65, 68); //W,A,S,D
+  //we will move pokemonBoy with up, down, left and right arrow keys
+  pokemonBoy = new Predator(100, 100, 5, color(66, 135, 245), 40, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW);
+  //our pokemons to be catched
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
@@ -32,22 +37,27 @@ function draw() {
   // Clear the background to black
   background(0);
 
-  // Handle input for the tiger
-  tiger.handleInput();
-
-  // Move all the "animals"
-  tiger.move();
+  // Handle input for the pokemonBoy and pokemonGirl
+  pokemonBoy.handleInput();
+  pokemonGirl.handleInput();
+  // Move all the "characters"
+  pokemonBoy.move();
+  pokemonGirl.move();
   antelope.move();
   zebra.move();
   bee.move();
 
-  // Handle the tiger eating any of the prey
-  tiger.handleEating(antelope);
-  tiger.handleEating(zebra);
-  tiger.handleEating(bee);
+  // Handle the pokemonBoy eating any of the prey
+  pokemonBoy.handleEating(antelope);
+  pokemonBoy.handleEating(zebra);
+  pokemonBoy.handleEating(bee);
+  pokemonGirl.handleEating(antelope);
+  pokemonGirl.handleEating(zebra);
+  pokemonGirl.handleEating(bee);
 
   // Display all the "animals"
-  tiger.display();
+  pokemonBoy.display();
+  pokemonGirl.display();
   antelope.display();
   zebra.display();
   bee.display();
