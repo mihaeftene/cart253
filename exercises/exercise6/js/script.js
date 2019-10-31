dbe8cb234e4bcf7cf7be290c9c24b55545b64e2b
mihaeftene <mihaeftene@users.noreply.github.com>
2019-10-30 23:29:19 -0400

    Ex6: FIX4: Removed the extra comma between 100 and 100 in function setup().The program thinks that theres another value in between 100 and 100 but its not the case so we remove it.

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -27,7 +27,8 @@ let bee;
 ////// FIXED: sepreate function and setup//////
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100, , 100, 5, color(200, 200, 0), 40);
+  /////FIXED: Extra comma between 100 and 100. (Remove the extra comma)
+  tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
